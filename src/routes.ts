c1923927c3aae67941b82bb8f0d6326e3b911177
danielbrtrindade <daniel_brt@hotmail.com>
2022-01-29 01:55:41 -0300

    Create Login

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClient = new AuthenticateClientController();
 
+routes.post("/authenticate", authenticateClient.handle);
 routes.post("/client/", createClientController.handle);
 
 export { routes };
